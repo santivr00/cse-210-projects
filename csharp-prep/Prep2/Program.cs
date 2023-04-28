b760b0cb7b01b75c3d29a46e0c24fb925449d16d
santivr00 <james00kva@gmail.com>
2023-04-28 14:48:22 +0200

    First part of the committed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Student's grade");
+        Console.Write("What's the student's final score? ");
+        string score = Console.ReadLine();
+        int grade = int.Parse(score);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("They're final grade is an A");
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            Console.WriteLine("They're final grade is a B");
+        }
+        else if (grade < 80 && grade >= 70)
+        {
+            Console.WriteLine("They're final grade is a C");
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            Console.WriteLine("They're final grade is a D");
+        }
+        else
+        {
+            Console.WriteLine("They're final grade is an F");
+        }
     }
 }
