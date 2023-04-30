0fb4bd3e3281fcaadd23b6aa6a911acd5852549c
santivr00 <james00kva@gmail.com>
2023-04-30 02:53:13 +0200

    Finished core requirements

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers;
+        numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userNumber = -1;
+
+        do
+        {
+            Console.Write("Enter number: ");            
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            numbers.Add(userNumber);
+        }while (userNumber != 0);
+       
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
