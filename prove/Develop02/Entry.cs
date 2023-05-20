9481483798baa403514bb5c435cd029e8d603e57
santivr00 <james00kva@gmail.com>
2023-05-20 15:11:27 +0200

    First attempt for Entry and PromptGenerator classes

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Entry{
+    public string _prompt;
+    public string _response;
+    public string _date;
+    public Entry(string prompt, string response, string date){
+        _prompt = prompt;
+        _response = response;
+        _date = date;
+    }
+}
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,25 @@
+using System;
+
+class PromptGenerator{
+    public List<string>prompts;
+    public Random random;
+    public PromptGenerator()
+    {
+        prompts = new List<string>()
+        {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+        };
+
+       random = new Random(); 
+    }
+
+    public string GetPrompt()
+    {
+        int index = random.Next(prompts.Count);
+        return prompts[index];
+    }
+}
